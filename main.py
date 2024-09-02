1d2c2b11379c3738addd399933349973ea8303f6
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-09-02 10:46:13 -0600

    feat: implement DPLL algorithm with test cases

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import utils.BruteForceAlg as BTa
+from utils.BruteForceAlg import fuerza_bruta_sat
+from utils.DPLLAlg import dpll
 
 if __name__ == "__main__":
-    print("Algoritmo de Fuerza Bruta \n")
-
     f_case1 = [ {"¬A"}, {"B", "C"} ]
     f_case2 = [ {"A"}, {"¬A", "B"}, {"¬B"} ]
 
-    resultado, asignacion = BTa.fuerza_bruta_sat(f_case1)
-    if resultado:
-        print(f"La fórmula es satisfacible con la asignación: \n{asignacion}")
+    use_dpll = False  # Si el valor es False se usará el alogoritmo de fuerza bruta
+
+    print("Algoritmo de Fuerza Bruta \n" if not use_dpll else "Algoritmo DPLL \n")
+
+    if use_dpll:
+        resultado, asignacion = dpll(f_case1, {})
     else:
-        print("La fórmula es insatisfacible")
+        resultado, asignacion = fuerza_bruta_sat(f_case1)
+
+    if resultado:
+        print(f"La fórmula es satisfacible con la asignación: \n{asignacion}\n")
+    else:
+        print("\nLa fórmula es insatisfacible")
