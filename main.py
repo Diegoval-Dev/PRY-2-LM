6c630744c0225a3ab5f5ae42ad51084671437f55
Nelson García Bravatti <gar22434@uvg.edu.gt>
2024-09-02 18:37:51 -0600

    feat: add for loop implementation

--- main.py
+++ main.py
@@ -2,19 +2,30 @@ from utils.BruteForceAlg import fuerza_bruta_sat
 from utils.DPLLAlg import dpll
 
 if __name__ == "__main__":
-    f_case1 = [ {"¬A"}, {"B", "C"} ]
-    f_case2 = [ {"A"}, {"¬A", "B"}, {"¬B"} ]
+    # ¬
 
-    use_dpll = False  # Si el valor es False se usará el alogoritmo de fuerza bruta
+    f_case = [  
+        [{"P"}, {"¬P"}],
+        [{"Q", "P", "¬P"}],
+        [{"¬P", "¬R", "¬S"}, {"¬Q", "¬P", "¬S"}],
+        [{"¬P", "¬Q"}, {"Q", "S"}, {"¬P", "S"}, {"¬Q", "S"}],
+        [{"¬P", "¬Q", "¬R"}, {"Q", "¬R", "P"}, {"¬P", "Q", "R"}],
+        [{"R"}, {"¬Q", "R"}, {"¬P", "Q", "¬R"}, {"Q"}]
+    ]
 
-    print("Algoritmo de Fuerza Bruta \n" if not use_dpll else "Algoritmo DPLL \n")
+    for formula in f_case:
+        print(f"Fórmula: {formula}")
 
-    if use_dpll:
-        resultado, asignacion = dpll(f_case1, {})
-    else:
-        resultado, asignacion = fuerza_bruta_sat(f_case1)
+        use_dpll = False  # Si el valor es False se usará el alogoritmo de fuerza bruta
 
-    if resultado:
-        print(f"La fórmula es satisfacible con la asignación: \n{asignacion}\n")
-    else:
-        print("\nLa fórmula es insatisfacible")
+        print("Algoritmo de Fuerza Bruta \n" if not use_dpll else "Algoritmo DPLL \n")
+
+        if use_dpll:
+            resultado, asignacion = dpll(formula, {})
+        else:
+            resultado, asignacion = fuerza_bruta_sat(formula)
+
+        if resultado:
+            print(f"La fórmula es satisfacible con la asignación: \n{asignacion}\n")
+        else:
+            print("\nLa fórmula es insatisfacible")
